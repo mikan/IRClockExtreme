d16b3f19885f572e51e29c4e98d70912e3cdcd85
mikan <yutakakato@icloud.com>
2015-01-10 18:07:18 +0900

    Supports port less.

--- cmd/src/main/java/net/aoringo/ircex/cmd/Commander.java
+++ cmd/src/main/java/net/aoringo/ircex/cmd/Commander.java
@@ -11,6 +11,7 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -49,17 +50,26 @@ public class Commander {
     }
 
     public void executePost(String command, String host) throws IOException {
+        Objects.requireNonNull(command);
+        Objects.requireNonNull(host);
+        String hostWithPort = host;
+        if (!host.contains(":")) {
+            hostWithPort += ":10000";
+        }
         HttpURLConnection connection = null;
         try {
             // Request
-            URL url = new URL("http://" + host + "/");
+            URL url = new URL("http://" + hostWithPort + "/");
+            LOG.log(Level.INFO, "URL: {0}", url.toString());
             connection = (HttpURLConnection) url.openConnection();
-            connection.setDoOutput(true);
             connection.setRequestMethod("POST");
+            connection.setInstanceFollowRedirects(false);
+            connection.connect();
             String parameter = "command=" + command;
             try (PrintWriter writer = new PrintWriter(connection.getOutputStream())) {
                 writer.print(parameter);
             }
+            LOG.log(Level.INFO, "Request sent.");
             // Response
             int responseCode = connection.getResponseCode();
             if (responseCode == HttpURLConnection.HTTP_OK) {
